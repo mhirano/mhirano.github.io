1cf6c9013fe1522caa66fcea4ed53c6cf30c4db6
mhirano <masahiro.dll@gmail.com>
2025-11-25 17:16:42 +0900

    avoid security challenge by cloudflare

--- _plugins/external-posts.rb
+++ _plugins/external-posts.rb
@@ -11,22 +11,31 @@ module ExternalPosts
       if site.config['external_sources'] != nil
         site.config['external_sources'].each do |src|
           p "Fetching external posts from #{src['name']}:"
-          xml = HTTParty.get(src['rss_url']).body
-          feed = Feedjira.parse(xml)
-          feed.entries.each do |e|
-            p "...fetching #{e.url}"
-            slug = e.title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
-            path = site.in_source_dir("_posts/#{slug}.md")
-            doc = Jekyll::Document.new(
-              path, { :site => site, :collection => site.collections['posts'] }
-            )
-            doc.data['external_source'] = src['name'];
-            doc.data['feed_content'] = e.content;
-            doc.data['title'] = "#{e.title}";
-            doc.data['description'] = e.summary;
-            doc.data['date'] = e.published;
-            doc.data['redirect'] = e.url;
-            site.collections['posts'].docs << doc
+          begin
+            response = HTTParty.get(src['rss_url'], headers: { 'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36' })
+            if response.code != 200
+              p "Error fetching #{src['rss_url']}: #{response.code}"
+              next
+            end
+            xml = response.body
+            feed = Feedjira.parse(xml)
+            feed.entries.each do |e|
+              p "...fetching #{e.url}"
+              slug = e.title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+              path = site.in_source_dir("_posts/#{slug}.md")
+              doc = Jekyll::Document.new(
+                path, { :site => site, :collection => site.collections['posts'] }
+              )
+              doc.data['external_source'] = src['name'];
+              doc.data['feed_content'] = e.content;
+              doc.data['title'] = "#{e.title}";
+              doc.data['description'] = e.summary;
+              doc.data['date'] = e.published;
+              doc.data['redirect'] = e.url;
+              site.collections['posts'].docs << doc
+            end
+          rescue => e
+            p "Error processing feed from #{src['name']}: #{e.message}"
           end
         end
       end
